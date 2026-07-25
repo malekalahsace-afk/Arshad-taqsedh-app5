4e5f0c5be2b0b6aa173b27d09f95fa063e1f0bcf
malekalahsace-afk <malekalahsace@gmail.com>
2026-07-25 08:44:54 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'taqseet-cache-75287db5';
+const CACHE_NAME = 'taqseet-cache-595e754d';
 const FILES_TO_CACHE = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
