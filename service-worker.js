ecdb8cb6d61caafb6fc8ddf4792529327a795d54
malekalahsace-afk <malekalahsace@gmail.com>
2026-08-04 01:32:50 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'taqseet-cache-0fe5cbb4';
+const CACHE_NAME = 'taqseet-cache-5b38cec0';
 const FILES_TO_CACHE = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
